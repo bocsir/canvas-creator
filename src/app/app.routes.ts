dd1d501d99cfbee48d3a5c84385bd79ae646f50b
bocsir <jacobwharris13@gmail.com>
2024-06-20 20:13:38 -0400

    -

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -3,6 +3,6 @@ import { HomeComponent } from './home/home.component';
 import { DesignComponent } from './design/design.component';
 
 export const routes: Routes = [
-    {path: 'home', component: HomeComponent},
+    {path: '', component: HomeComponent},
     {path: 'design', component: DesignComponent}
 ];
